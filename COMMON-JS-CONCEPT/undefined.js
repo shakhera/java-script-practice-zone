8fa96ff4783548d044a945f5a5f062137eee6e95
shakhera <shakherakhanom024@gmail.com>
2023-09-06 20:21:53 +0600

    way to get undefined

---
+++ COMMON-JS-CONCEPT/undefined.js
@@ -0,0 +1,13 @@
+/*
+* way to get undefined
+
+1. variable -> not initialized 
+2. funtion -> no return
+3. parameter that is not passed
+4. if return nothing on right side
+5. property that not axist on object
+6.accessing array element outside of the index range
+7. delete an element insise a array, in this situation you use slice...
+8. ser a value directly to undefined
+
+*/
