bc2bc8acb9f24827f99844e9db1eccda16f3892e
shakhera <shakherakhanom024@gmail.com>
2023-09-06 21:41:01 +0600

    == and ===

--- COMMON-JS-CONCEPT/equal.js
+++ COMMON-JS-CONCEPT/equal.js
@@ -1,7 +1,10 @@
-const n1 = 2;
-const n2 = '2';
+// tai always triple(===)use korbo
+//equal comparisn doesn't work for non-premitive
+const n1 = [1, 2];
+const n2 = [1, 2];
 // double equal num-num, string-string check kore (2--'2' not same) bt
 // triple equal num-string o check kore( 2==='2' are same)
+
 if (n1 === n2) {
     console.log('they are equal');
 }
