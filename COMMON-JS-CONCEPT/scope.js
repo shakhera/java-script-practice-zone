8545927e3d1d9779cf084fcc096b4753598dcb6d
shakhera <shakherakhanom024@gmail.com>
2023-09-06 21:50:36 +0600

    Block scope

---
+++ COMMON-JS-CONCEPT/scope.js
@@ -0,0 +1,9 @@
+// variable kono block ar vaire kaj korbe na
+function add(a, b) {
+    const total = a + b;
+    console.log(a, b);
+    return total;
+}
+// console.log(a,b);
+// console.log(total);
+add(8, 5);
